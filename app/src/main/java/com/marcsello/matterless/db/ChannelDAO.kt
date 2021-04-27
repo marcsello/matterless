2d0ef537dbd3a790a96e14b2537ca1c4b60e7891
marcsello <punkosdmarcell@rocketmail.com>
2021-04-28 01:25:34 +0200

    Added ordering to channels

--- app/src/main/java/com/marcsello/matterless/db/ChannelDAO.kt
+++ app/src/main/java/com/marcsello/matterless/db/ChannelDAO.kt
@@ -4,19 +4,19 @@ import androidx.room.*
 
 @Dao
 interface ChannelDAO {
-    @Query("SELECT * FROM channels")
+    @Query("SELECT * FROM channels ORDER BY last_read_at DESC")
     suspend fun getChannels(): List<Channel>
 
-    @Query("SELECT * FROM channels WHERE team_id = :teamId")
+    @Query("SELECT * FROM channels WHERE team_id = :teamId ORDER BY last_read_at DESC")
     suspend fun getChannelsOfTeam(teamId: String): List<Channel>
 
-    @Query("SELECT * FROM channels WHERE last_post_at > :since")
+    @Query("SELECT * FROM channels WHERE last_post_at > :since ORDER BY last_read_at DESC")
     suspend fun getChannelsUpdatedSince(since: Long): List<Channel>
 
-    @Query("SELECT * FROM channels WHERE team_id = :teamId AND last_post_at > :since")
+    @Query("SELECT * FROM channels WHERE team_id = :teamId AND last_post_at > :since ORDER BY last_read_at DESC")
     suspend fun getChannelsOfTeamUpdatedSince(teamId: String, since: Long): List<Channel>
 
-    @Query("SELECT * FROM channels WHERE team_id = :teamId AND last_post_at > last_read_at")
+    @Query("SELECT * FROM channels WHERE team_id = :teamId AND last_post_at > last_read_at ORDER BY last_read_at DESC")
     suspend fun getChannelsOfTeamWithUnread(teamId: String): List<Channel>
 
     @Insert
