2d0ce47873e336890856fdd7f99cf14cfc019699
marcsello <punkosdmarcell@rocketmail.com>
2021-04-27 21:39:17 +0200

    Added profile activity

---
+++ app/src/main/java/com/marcsello/matterless/ui/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.marcsello.matterless.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.marcsello.matterless.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
