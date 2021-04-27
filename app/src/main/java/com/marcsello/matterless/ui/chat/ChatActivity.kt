0e9dde802a84556fae36faf513bd890a29accb28
marcsello <punkosdmarcell@rocketmail.com>
2021-04-27 22:13:49 +0200

    Added chat activity

---
+++ app/src/main/java/com/marcsello/matterless/ui/chat/ChatActivity.kt
@@ -0,0 +1,11 @@
+package com.marcsello.matterless.ui.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
