06e19d2b28b4424fdf89a3630f8641c1be36c549
marcsello <punkosdmarcell@rocketmail.com>
2021-04-27 22:19:33 +0200

    Added missing import

--- app/src/main/java/com/marcsello/matterless/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/marcsello/matterless/ui/chat/ChatActivity.kt
@@ -2,6 +2,7 @@ package com.marcsello.matterless.ui.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.marcsello.matterless.R
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
